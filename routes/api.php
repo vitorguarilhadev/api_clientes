86b0581b4714d9b94513fe593119dd9394284348
vitorguarilha <72364854+vitorguarilha@users.noreply.github.com>
2023-02-08 14:01:49 -0300

    feature: criando rotas de listagem e deleção de clientes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ClientesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/listarClientes', [ClientesController::class, 'listarClientes']);
+Route::get('/listarClientes/{id}', [ClientesController::class, 'listarClientesById']);
+Route::delete('/deletarCliente/{id}', [ClientesController::class, 'deletarCliente']);
+
+
