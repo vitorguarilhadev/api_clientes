e9a2f11d5cc448d458a703d20639106a907c6a0d
vitorguarilha <72364854+vitorguarilha@users.noreply.github.com>
2023-02-08 14:02:19 -0300

    feature: criando Models de clientes e endereços

---
+++ app/Models/Clientes.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Clientes extends Model
+{
+
+    protected $table = 'clientes';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'foto',
+        'nome',
+        'nome_mãe',
+        'data_nascimento',
+        'cpf',
+        'cns',
+        'endereco_id'
+    ];
+
+    public function endereco()
+    {
+        return $this->hasOne('App\Models\Enderecos','id','endereco_id');
+    }
+
+}
---
+++ app/Models/Enderecos.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Enderecos extends Model
+{
+
+    protected $table = 'enderecos';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'cep',
+        'endereco',
+        'numero',
+        'complemento',
+        'bairro',
+        'cidade',
+        'estado'
+    ];
+
+}
